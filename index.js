f1125a6fb4d9834630f663e1eeb2c398de268f06
krivera1015 <krivera1015@gmail.com>
2018-06-24 02:23:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 funkyFunction()
 var theFunk = function(){
-  return function(){
+  return funkyFunction(){
   }
 }
